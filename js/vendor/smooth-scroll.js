581f3446ced2f155d12a5c7abb707a05ab67fbcb
Taras <tarasbgluck@gmail.com>
2018-08-15 20:45:58 +0000

    Initial

--- js/vendor/smooth-scroll.js
+++ js/vendor/smooth-scroll.js
@@ -1,11 +1,11 @@
 $(function() {
     $('.smooth').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
+        const target = $(this.getAttribute('href'));
         if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 2000);
+            }, 1000);
         }
     });
 });
